67b42f5bc61c355f7e1f2846791f2e4ac9090e3e
Yazan Alkhatib <yazankhatib97@gmail.com>
2022-11-08 13:36:49 +0400

    fix: exports from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Grid } from './components';
 import './index.css';
+
+export * from './components/grid';
+export * from './components/tabs';
 
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
@@ -8,47 +10,6 @@ import './index.css';
  * A custom Thing component. Neat!
  */
 
-const columns = [
-  { field: 'firstName', header: 'Firstname', width: '1' },
-  { field: 'lastName', header: 'Lastname', width: '1' },
-  { field: 'email', header: 'Email', width: '1' },
-  { field: 'username', header: 'Username', width: '1' },
-  { field: 'phoneNumber', header: 'Phone Number', width: '1' },
-  { field: 'status', header: 'Status', width: '1' },
-];
-
-const data = [
-  {
-    id: '03788639-828c-4571-88df-e65773239ddc',
-    username: 'josefine',
-    firstName: 'josefine',
-    lastName: 'Skov Pedersen',
-    email: 'jsp@relateit.dk',
-    phoneNumber: '0546626822',
-    status: 'active',
-  },
-  {
-    id: '5b3ecb2c-3e02-4d88-b3ed-5ab107e453f7',
-    username: 'yazan',
-    firstName: 'Yazan',
-    lastName: 'Alkhatib',
-    email: 'yaa@relateit.dk',
-    phoneNumber: '0546626822',
-    status: 'active',
-  },
-  {
-    id: '5bffef1d-9856-4428-8682-1c5eab9f10b7',
-    username: 'john3',
-    firstName: 'john',
-    lastName: 'doe',
-    email: 'johndoe3@email.com',
-    phoneNumber: '0522232452',
-    status: 'active',
-  },
-];
-
 export const App: React.FC = () => {
-  return (
-    <Grid columns={columns} data={data} resource="users" loading={false} />
-  );
+  return <h1>App</h1>;
 };
