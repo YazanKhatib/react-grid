7f6ae25b4679734ee432d72e62e665ccb1d21035
Yazan Alkhatib <yazankhatib97@gmail.com>
2023-06-06 17:41:08 +0400

    feat: update configuration file

--- packages/grid/vite.config.js
+++ packages/grid/vite.config.js
@@ -4,12 +4,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
