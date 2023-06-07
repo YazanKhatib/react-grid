ebb4aa8db654b5c04f23ab8c2e222a17765dfbc8
Yazan Alkhatib <yazankhatib97@gmail.com>
2023-06-07 14:05:20 +0400

    vite configuration

--- packages/grid/vite.config.js
+++ packages/grid/vite.config.js
@@ -4,7 +4,12 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
